dc0ce5e28e87a37854f3713533c220d1f973c92d
chiboycalix <igwechinonso77@gmail.com>
2021-09-13 12:43:52 +0100

    add e2e test with cypress

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import Logo from '../../Assets/logo.svg'
-import './styles.scss';
+import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import Logo from "../../Assets/spacex.svg";
+import "./styles.scss";
 
 const Header: React.FC = () => {
   const history = useHistory();
@@ -9,11 +9,11 @@ const Header: React.FC = () => {
 
   return (
     <header>
-      <img src={Logo} alt="logo" onClick={() => history.push('/')} />
+      <img src={Logo} alt="logo" onClick={() => history.push("/")} />
       <div className="current__page">
-        <h1>{location.pathname.split('/')[1].toUpperCase()}</h1>
+        <h1>{location.pathname.split("/")[1].toUpperCase()}</h1>
       </div>
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,40 +1,47 @@
 /** @format */
 
-import React from 'react';
-import SpaceDragon from '../../Assets/spacex_dragon.png';
-import { useHistory } from 'react-router-dom';
-import './styles.scss';
+import React from "react";
+import SpaceDragon from "../../Assets/spacex_dragon.png";
+import { useHistory } from "react-router-dom";
+import { Button } from "../../Components";
+import "./styles.scss";
 
 const Home: React.FC = () => {
-  const history = useHistory()
+  const history = useHistory();
 
   const handleViewLaunch = () => {
-    history.push('/launches')
-  }
+    history.push("/launches");
+  };
   const handleCompareLaunch = () => {
-    history.push('/compare')
-  }
+    history.push("/compare");
+  };
   return (
     <div className="home__wrapper">
       {/* hero section */}
       <div className="hero__wrapper">
         <div className="hero__left-section">
-          <h1>EXPLORE EXPEDITIONS IN THE UNIVERSE</h1>
+          <h1>
+            EXPLORE EXPEDITIONS IN THE <span>UNIVERSE</span>{" "}
+          </h1>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
             quas debitis aspernatur ipsa voluptatum?
           </p>
           <div className="box__wrapper">
-            <div className="box" onClick={handleViewLaunch}>
-              <p>See Launch Info</p>
-            </div>
-            <div className="box" onClick={handleCompareLaunch}>
-              <p>Compare Launch Info</p>
-            </div>
+            <Button type="primary" onClick={handleViewLaunch}>
+              Launches
+            </Button>
+            <Button type="secondary" onClick={handleCompareLaunch}>
+              Compare
+            </Button>
           </div>
         </div>
         <div className="hero__right-section">
-          <img src={SpaceDragon} alt="SpaceDragon" className="space__dragon-image"/>
+          <img
+            src={SpaceDragon}
+            alt="SpaceDragon"
+            className="space__dragon-image"
+          />
         </div>
       </div>
     </div>
